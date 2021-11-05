0a693134137ae3e5a33fa71db3c7844319a4f57c
tim-dv <inqdtv@gmail.com>
2021-11-05 18:53:16 +0300

    HW 1 edited

--- Python_HW_1.py
+++ Python_HW_1.py
@@ -1,42 +1,42 @@
 # 1) Создать переменную типа String
-s = "Hi!"
-print(type(s), s)
+var_string = "Hi!"
+print(type(var_string), var_string)
 # 2) Создать переменную типа Integer
-i = 379
-print(type(i), i)
+var_integer = 379
+print(type(var_integer), var_integer)
 # 3) Создать переменную типа Float
-f = 31.79
-print(type(f), f)
+var_float = 31.79
+print(type(var_float), var_float)
 # 4) Создать переменную типа Bytes
-b = bytes(13)
-print(type(b), b)
+var_bytes = bytes(13)
+print(type(var_bytes), var_bytes)
 # 5) Создать переменную типа List
-l = ['inq', 290, 67, ['df57'], 13]
-print(type(l), l)
+var_list = ["inq", 290, 67, ["df57"], 13]
+print(type(var_list), var_list)
 # 6) Создать переменную типа Tuple
-t = ("dfg", 97, "fgh56")
-print(type(t), t)
+var_tuple = ("dfg", 97, "fgh56")
+print(type(var_tuple), var_tuple)
 # 7) Создать переменную типа Set
-set = set("friday")
-print(type(set), set)
+var_set = set("friday")
+print(type(var_set), var_set)
 # 8. Создать переменную типа Frozen set
-frs = frozenset("qwerty")
-print(type(frs), frs)
+var_frozenset = frozenset("qwerty")
+print(type(var_frozenset), var_frozenset)
 # 9) Создать переменную типа Dict
-dct = {"age": 36,
-       "name": "Tim"}
-print(type(dct), dct)
+var_dict = {"age": 36,
+            "name": "Tim"}
+print(type(var_dict), var_dict)
 # 11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные.
 # Вывести в консоль.
-c = "Hello, "
-d = "everyone"
-e = c + d
+var_1 = "Hello, "
+var_2 = "everyone"
+e = var_1 + var_2
 print(e)
 # 12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
 name = "Tim"
 age = 36
-print(name + ", " + str(age))
+print(name, (age))
 # 13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
 first = "five"
 second = 24
-print(first + " + " + str(second))
+print(first + str(second))
