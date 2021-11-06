e97de0ae177b35895b3347d684c9d01fa109d913
tim-dv <inqdtv@gmail.com>
2021-11-06 17:46:44 +0300

    Edited HW 2

--- Python_HW_2.py
+++ Python_HW_2.py
@@ -12,7 +12,10 @@ result_sum = item_1 + item_2
 print("result_sum =", result_sum)
 
 # 5. Создать переменную result_subtr в которой вы вычитаете большую по значению переменную из меньшей по значению.
-result_subtr = item_2 - item_1
+if item_1 > item_2:
+    result_subtr = item_2 - item_1
+else:
+    result_subtr = item_1 - item_2
 
 # 6. Вывести result_subtr в консоль.
 print("result_subtr =", result_subtr)
