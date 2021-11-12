26bcbfcfbf6fa77505f21cc351079d07ed83b3f0
tim-dv <inqdtv@gmail.com>
2021-11-12 21:45:45 +0300

    Edited HW 3

--- Python_HW_3.py
+++ Python_HW_3.py
@@ -65,7 +65,7 @@ usd_byn_rate = 2.46
 # 20) Сделать if в котором будет условие: если mult_int больше comp_item, то вывести в консоль
 # (“Переменная mult_int больше”, comp_item)
 if mult_int > comp_item:
-    print('Переменная ' + str(mult_int) + ' больше', comp_item)
+    print('Переменная mult_int больше', comp_item)
 
 
 # 21) Создать переменную div_int в которй разделить int_item на 2
@@ -74,7 +74,7 @@ div_int = int_item / 2
 # 22) Сделать if в котором будет условие: если div_int меньше comp_item, то вывести в консоль
 # (“Переменная div_int меньше”, comp_item)
 if div_int < comp_item:
-    print('Переменная ' + str(div_int) + ' меньше', comp_item)
+    print('Переменная div_int меньше', comp_item)
 
 
 # 23) Создать переменную item_1 в которй прибавить 10 к переменной int_item
@@ -83,74 +83,71 @@ item_1 = int_item + 10
 # 24) Сделать if в котором будет условие: если item_1 меньше comp_item, то вывести в консоль
 # (“Переменная item_1 меньше”, comp_item), иначе, вывести в консоль (“Переменная item_1 больше или равна”, comp_item)
 if item_1 < comp_item:
-    print('Переменная ' + str(item_1) + ' меньше', comp_item)
+    print('Переменная item_1 меньше', comp_item)
 else:
-    print('Переменная ' + str(item_1) + ' больше или равна', comp_item)
+    print('Переменная item_1 больше или равна', comp_item)
 
 # 25) Сделать if в котором будет условие: если item_2, то вывести в консоль (“Переменная item_2 = ”, item_2),
 # иначе, вывести в консоль (“Переменная item_2 = ”, item_3)
 if item_2:
-    print('Переменная ' + str(item_2) + ' =', item_2)
+    print('Переменная item_2 =', item_2)
 else:
-    print('Переменная ' + str(item_2) + ' =', item_3)
+    print('Переменная item_2 =', item_3)
 
 # 26) Сделать if в котором будет условие: если item_3, то вывести в консоль (“Переменная item_3 = ”, item_2),
 # иначе, вывести в консоль (“Переменная item_3 = ”, item_3)
 if item_3:
-    print('Переменная ' + str(item_3) + ' =', item_2)
+    print('Переменная item_3 =', item_2)
 else:
-    print('Переменная ' + str(item_3) + ' =', item_3)
+    print('Переменная item_3 =', item_3)
 
 # 27) Сделать if в котором будет условие: если item_5, то вывести в консоль (“Переменная item_5 = ”, item_5),
 # иначе, вывести в консоль (“Переменная item_5 = ”, item_4)
 if item_5:
-    print('Переменная ' + str(item_5) + ' =', item_5)
+    print('Переменная item_5 =', item_5)
 else:
-    print('Переменная ' + str(item_5) + ' =', item_4)
+    print('Переменная item_5 =', item_4)
 
 # 28) Сделать if в котором будет условие: если item_4, то вывести в консоль (“Переменная item_4 = ”, item_5),
 # иначе, вывести в консоль (“Переменная item_4 = ”, item_4)
 if item_4:
-    print('Переменная ' + str(item_4) + ' =', item_5)
+    print('Переменная item_4 =', item_5)
 else:
-    print('Переменная ' + str(item_4) + ' =', item_4)
+    print('Переменная item_4 =', item_4)
 
 
 # 29) Создать переменную currency_convertor со значением item_2
-currency_convertor = item_2
-
 # 30) Сделать if в котором будет условие: если currency_convertor, то выполнять следующие шаги задания, иначе,
 # вывести в консоль (“Переменная currency_convertor = ”, item_3)
-if currency_convertor:
-
 # 31) Внутри if currency_convertor сделать следующие If условия :
 # 31.1) Создать переменную currency_usd со значением usd_item
-    currency_convertor = usd_item
-
 # 31.2) Создать переменную target_currency со значением eur_item
-    target_currency = eur_item
-
 # 31.3) Создать переменную target_currency_amount значением 50
-    target_currency_amount = 50
-
 # 31.4) Создать переменную currency_result со значением 0
-    currency_result = 0
-
 # 31.5) Сделать if в котором будет условие: если target_currency равен ‘eur’, то в теле этого if в значении
 # переменной currency_result высчитать сколько долларов получится при target_currency_amount и usd_eur_rate.
 # Результат вывести в консоль (target_currency_amount, eur_item, “=”, currency_result, usd_item)
+# 31.6) Сделать elif в котором будет условие: если target_currency равен ‘uah’, то в теле этого if в значении
+# переменной currency_result высчитать сколько долларов получится при target_currency_amount и usd_uah_rate.
+# Результат вывести в консоль (target_currency_amount, uah_item, “=”, currency_result, uah_item)
+# 31.7) Сделать elif с остальными валютами
+# 31.8) Последним оставить else, при выполнений которого в консоль выведется (“Unknown currency”)
+
+currency_convertor = item_2
+if currency_convertor:
+    currency_convertor = usd_item
+    target_currency = eur_item
+    target_currency_amount = 50
+    currency_result = 0
+
     if target_currency == 'eur':
         currency_result = target_currency_amount / usd_eur_rate
         print(target_currency_amount, eur_item, '=', currency_result, usd_item)
 
-# 31.6) Сделать elif в котором будет условие: если target_currency равен ‘uah’, то в теле этого if в значении
-# переменной currency_result высчитать сколько долларов получится при target_currency_amount и usd_uah_rate.
-# Результат вывести в консоль (target_currency_amount, uah_item, “=”, currency_result, uah_item)
     elif target_currency == 'uah':
         currency_result = target_currency_amount / usd_uah_rate
         print(target_currency_amount, uah_item, '=', currency_result, usd_item)
 
-# 31.6) Сделать elif с остальными валютами
     elif target_currency == 'byn':
         currency_result = target_currency_amount / usd_byn_rate
         print(target_currency_amount, byn_item, '=', currency_result, usd_item)
@@ -167,7 +164,6 @@ if currency_convertor:
         currency_result = target_currency_amount / usd_usd_rate
         print(target_currency_amount, usd_item, '=', currency_result, usd_item)
 
-    # 31.7) Последним оставить else, при выполнений которого в консоль выведется (“Unknow currency”)
     else:
         print('Unknown currency')
 
